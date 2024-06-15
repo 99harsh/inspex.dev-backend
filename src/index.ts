297dc57bb7040742b941038b4834ec6f09e4a053
= <=>
2024-06-15 09:04:13 +0530

    Socket disconnect and invalid room code logic added

--- src/index.ts
+++ src/index.ts
@@ -26,8 +26,10 @@ wss.on('connection', (ws: any) => {
 
         if (decodedObject.event == "create_room") {
             const roomId = generateRoomId();
+            const clientId = generateClientId();
+            ws.room_owner = "host";
+            ws.client_id = clientId;
             ws.room_id = roomId;
-            ws.client_id = decodedObject.client_id;
             if (!rooms[roomId]) {
                 rooms[roomId] = [];
             }
@@ -37,31 +39,71 @@ wss.on('connection', (ws: any) => {
             const response = {
                 event: "exchange_dom",
                 room_id: roomId,
-                processed_dom: updatedDomString
+                processed_dom: updatedDomString,
+                client_id: clientId
             }
-
-            ws.dom = updatedDomString;
-            rooms[roomId] = [ws];
+            rooms[roomId] = { host: ws };
+            console.log("Room Created:- ", clientId, roomId);
             ws.send(JSON.stringify(response));
-            console.log(rooms)
-
-
         } else if (decodedObject.event == "join_room") {
-
             if (rooms.hasOwnProperty(decodedObject.room_id)) {
-                const response = {
-                    room_id: decodedObject.room_id,
-                    event: "exchange_dom",
-                    processed_dom: rooms[decodedObject.room_id][0]?.dom,
+                const client_id = generateClientId();
+                ws.client_id = client_id;
+                ws.room_id = decodedObject.room_id;
+                ws.room_owner = "client";
+                if(rooms[decodedObject.room_id]?.client){   
+                    rooms[decodedObject.room_id].client.push(ws);
+                }else{
+                    rooms[decodedObject.room_id].client = [ws];
                 }
-                rooms[decodedObject.room_id].push(ws);
-                ws.send(JSON.stringify(response))
+                rooms[decodedObject.room_id].host.send(JSON.stringify({ event: "request_dom", client_id: client_id }));
+                console.log("User Joined:- ", client_id, decodedObject.room_id)
+            }else{
+                ws.send(JSON.stringify({event: "error", statusCode: 404, messsage: "Room Not Found"}))
             }
-        } else if (decodedObject.event == "listen_change" || true) {
-            if (rooms.hasOwnProperty(decodedObject.room_id) && decodedObject?.room_owner == "host" && rooms[decodedObject.room_id]?.length > 1) {
-                rooms[decodedObject.room_id][1].send(JSON.stringify(decodedObject));
-            } else if (rooms.hasOwnProperty(decodedObject.room_id) && decodedObject?.room_owner == "client" && rooms[decodedObject.room_id]?.length > 1) {
-                rooms[decodedObject.room_id][0].send(JSON.stringify(decodedObject));
+        } else if (decodedObject.event == "requested_dom") {
+            if (rooms.hasOwnProperty(decodedObject.room_id)) {
+                const clients = rooms[decodedObject.room_id].client;
+                for (let client of clients) {
+                    if (client.client_id == decodedObject.client_id) {
+                        client.send(JSON.stringify({event: "exchange_dom", room_id: decodedObject.room_id, client_id: client.client_id, processed_dom: decodedObject.dom}))
+                    }
+                }
+            }
+        } else if (decodedObject.event == "listen_change" ||decodedObject.event == "lock_element") {
+            if(rooms.hasOwnProperty(decodedObject.room_id)){
+                const host = rooms[decodedObject.room_id].host;
+                const clients = rooms[decodedObject.room_id]?.client;
+                
+                if(decodedObject.room_owner != "host"){
+                    host.send(JSON.stringify(decodedObject));
+                }
+
+                if(clients){
+                    for(let client of clients){
+                        if(client.client_id != decodedObject.client_id){
+                            client.send(JSON.stringify(decodedObject));
+                        }
+                    }
+                }
+            }
+        }
+
+    })
+
+    ws.on('close', (code:any, reason:any) => {
+        console.log("User Disconnected:- ", ws.client_id, ws.room_id);
+        if(rooms[ws.room_id]){
+            if(rooms[ws.room_id]?.client?.length > 0 && ws.room_owner == "host"){
+                rooms[ws.room_id].host = rooms[ws.room_id].client[0];
+                rooms[ws.room_id].client.splice(0, 1);
+            }else if(rooms[ws.room_id]?.client?.length > 0 && ws.room_owner == "client"){
+                const index = rooms[ws.room_id].client.findIndex((o:any) => o.client_id == ws.client_id);
+                if(index != -1){
+                    rooms[ws.room_id].client.splice(index, 1);
+                }
+            }else{
+                delete rooms[ws.room_id];
             }
         }
 
@@ -77,7 +119,6 @@ const assignUniqueIds = (element: any) => {
     if (!element.hasAttribute('data-unique-id')) {
         element.setAttribute('data-unique-id', generateUniqueId());
     }
-    console.log("ELEMENT", element)
     Array.from(element.children).forEach(assignUniqueIds);
 }
 
@@ -95,6 +136,14 @@ const generateRoomId = (length = 5) => {
     }
 
     return randomBytes(length / 2).toString("hex");
+}
+
+const generateClientId = (length = 5) => {
+    if (length % 2 !== 0) {
+        length++;
+    }
+
+    return "INSPEX-" + randomBytes(length / 2).toString("hex");
 }
 
 server.listen(8080, () => {
